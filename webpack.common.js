3741f2cdb266bd0b2b3a1727fa27c676e073d22c
jeanmolossi <jean.carlosmolossi@gmail.com>
2020-12-31 15:07:16 -0300

    chore: eslint config

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,17 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: "./src/main/index.tsx",
+  entry: './src/main/index.tsx',
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "main-bundle-[hash].js",
-    publicPath: "/",
+    path: path.join(__dirname, 'dist'),
+    filename: 'main-bundle-[hash].js',
+    publicPath: '/',
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", "scss"],
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
     alias: {
-      "@": path.join(__dirname, "src"),
+      '@': path.join(__dirname, 'src'),
     },
   },
   plugins: [new CleanWebpackPlugin()],
