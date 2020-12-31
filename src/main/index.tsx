8a5c4285f619b43bff0d851b992985aacc73e7ed
jeanmolossi <jean.carlosmolossi@gmail.com>
2020-12-31 18:33:00 -0300

    fix: imports

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,5 +1,5 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Router } from '@/main/routes/router';
 
 ReactDOM.render(<Router />, document.getElementById('main'));
