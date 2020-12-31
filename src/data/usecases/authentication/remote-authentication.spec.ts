09fbe41c951f850d375fc469d34742db721940e8
jeanmolossi <jean.carlosmolossi@gmail.com>
2020-12-31 16:10:17 -0300

    test: tests RemoteAuthentication calls httpPostClient

---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,23 @@
+import { HttpPostClient } from '@/data/protocols/http/http-post-client';
+import { RemoteAuthentication } from './remote-authentication';
+
+describe('RemoteAuthentication', () => {
+  test('Should call HttpPostClient with correct URL', async () => {
+    class HttpPostClientSpy implements HttpPostClient {
+      url?: string;
+
+      async post(url: string): Promise<void> {
+        this.url = url;
+        return Promise.resolve();
+      }
+    }
+
+    const url = 'valid-url';
+    const httpPostClient = new HttpPostClientSpy();
+    const sut = new RemoteAuthentication(url, httpPostClient);
+
+    await sut.auth();
+
+    expect(httpPostClient.url).toBe(url);
+  });
+});
