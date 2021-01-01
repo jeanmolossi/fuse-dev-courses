d858944982341878c3b875593f50aae1062b5e35
jeanmolossi <jean.carlosmolossi@gmail.com>
2020-12-31 21:37:23 -0300

    test: ensure FetchHttpClient calls fetch with correct URL

---
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import faker from 'faker';
+import { AxiosHttpClient } from './axios-http-client';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe('FetchHttpClient', () => {
+  test('Should call fetch with correct URL', async () => {
+    const url = faker.internet.url();
+
+    const sut = new AxiosHttpClient();
+
+    await sut.post({ url });
+
+    expect(mockedAxios).toHaveBeenCalledWith(url);
+  });
+});
