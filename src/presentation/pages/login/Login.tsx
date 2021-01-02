a743c6a5376083a68c1ac0d68e35bf51d37f5575
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-02 10:15:17 -0300

    chore: login page

--- src/presentation/pages/login/Login.tsx
+++ src/presentation/pages/login/Login.tsx
@@ -4,7 +4,14 @@ import styles from './styles.scss';
 const Login = (): JSX.Element => {
   return (
     <div className={styles.login}>
-      <h1>Hello World</h1>
+      <header className={styles.header}>
+        <img src="" alt="" />
+        <h1>Plataforma</h1>
+      </header>
+
+      <div className={styles.form}>Form</div>
+
+      <footer className={styles.footer}>footer</footer>
     </div>
   );
 };
