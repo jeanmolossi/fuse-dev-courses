06ea0f8f96d0799f7963abf250c8d386cc2261c4
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-03 19:49:06 -0300

    test: create Login test

--- src/presentation/pages/login/Login.spec.tsx
+++ src/presentation/pages/login/Login.spec.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { render, RenderResult } from '@testing-library/react';
+import {
+  render,
+  RenderResult,
+  fireEvent,
+  cleanup,
+} from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Validation } from '@/presentation/protocols/validation';
 import Login from './Login';
@@ -11,8 +16,10 @@ type SutTypes = {
 
 class ValidationSpy implements Validation {
   errorMessage: string;
+  input: unknown;
 
   validate(input: unknown): string {
+    this.input = input;
     return this.errorMessage;
   }
 }
@@ -21,7 +28,7 @@ const makeSut = (): SutTypes => {
   const validationSpy = new ValidationSpy();
   const sut = render(
     <BrowserRouter>
-      <Login />
+      <Login validation={validationSpy} />
     </BrowserRouter>,
   );
 
@@ -32,16 +39,49 @@ const makeSut = (): SutTypes => {
 };
 
 describe('Login Component', () => {
-  afterEach(() => {
-    //
-  });
+  afterEach(cleanup);
 
   test('Should start with initial state', () => {
     const { sut } = makeSut();
-    const emailField = sut.getByTestId('email-email-field');
-    const passwordField = sut.getByTestId('password-password-field');
+    const emailField = sut
+      .getByTestId('email-email-field')
+      .querySelector('input');
+    const passwordField = sut
+      .getByTestId('password-password-field')
+      .querySelector('input');
+    const submitButton = sut.getByTestId('submit-button') as HTMLButtonElement;
 
     expect(!!emailField).toBe(true);
+    expect(emailField.nodeValue).toBeNull();
     expect(!!passwordField).toBe(true);
+    expect(passwordField.nodeValue).toBeNull();
+    expect(!!submitButton).toBe(true);
+    expect(submitButton.disabled).toBe(true);
+  });
+
+  test('Should call Validation with correct value', () => {
+    const { sut, validationSpy } = makeSut();
+
+    const emailField = sut
+      .getByTestId('email-email-field')
+      .querySelector('input');
+
+    fireEvent.input(emailField, {
+      target: { value: 'valid-email' },
+    });
+
+    expect(validationSpy.input).toEqual({
+      email: 'valid-email',
+    });
+
+    const passwordField = sut
+      .getByTestId('password-password-field')
+      .querySelector('input');
+
+    fireEvent.input(passwordField, { target: { value: 'valid-password' } });
+
+    expect(validationSpy.input).toEqual({
+      password: 'valid-password',
+    });
   });
 });
