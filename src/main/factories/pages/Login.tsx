3383e10c89e75b76026443ec0ff479ce96f4bc8f
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-03 17:06:51 -0300

    chore: login page presentation and login page factory

---
+++ src/main/factories/pages/Login.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Login } from '@/presentation/pages';
+
+const LoginFactory: React.FC = () => {
+  return <Login />;
+};
+
+export default LoginFactory;
--- src/presentation/pages/login/Login.tsx
+++ src/presentation/pages/login/Login.tsx
@@ -1,17 +1,104 @@
 import React from 'react';
+import {
+  Card,
+  CardContent,
+  Typography,
+  FormControl,
+  FormControlLabel,
+  Checkbox,
+  Divider,
+} from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { TextInput, Button } from '@/presentation/components';
 import styles from './styles.scss';
 
 const Login = (): JSX.Element => {
   return (
     <div className={styles.login}>
-      <header className={styles.header}>
-        <img src="" alt="" />
-        <h1>Plataforma</h1>
-      </header>
+      <div>
+        <Card>
+          <CardContent className={styles.card__content}>
+            <Typography variant="h6">Acesse sua conta</Typography>
 
-      <div className={styles.form}>Form</div>
+            <form
+              name="loginForm"
+              noValidate
+              onSubmit={() => {
+                //
+              }}
+              className={styles.form__section}
+            >
+              <TextInput
+                name="email"
+                label="Email"
+                type="email"
+                required
+                variant="outlined"
+              />
 
-      <footer className={styles.footer}>footer</footer>
+              <TextInput
+                name="password"
+                label="Senha"
+                type="password"
+                required
+                variant="outlined"
+              />
+
+              <div className={styles.password__remeber}>
+                <FormControl>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        name="remember"
+                        checked={true}
+                        onChange={() => {
+                          //
+                        }}
+                      />
+                    }
+                    label="Manter-me logado"
+                  />
+                </FormControl>
+
+                <Link to="/pages/auth/forgot-password">
+                  Esqueceu sua senha?
+                </Link>
+              </div>
+
+              <Button
+                variant="contained"
+                color="primary"
+                aria-label="Entrar"
+                disabled
+                fullWidth
+              >
+                Entrar
+              </Button>
+            </form>
+
+            <div className={styles.form__divider}>
+              <Divider light />
+              <span>OU</span>
+              <Divider light />
+            </div>
+
+            <div className={styles.buttons_container}>
+              <Button variant="outlined" color="primary" size="small">
+                Acesse com o Google
+              </Button>
+
+              <Button variant="outlined" color="primary" size="small">
+                Acesse com o Facebook
+              </Button>
+            </div>
+
+            <div className={styles.register__section}>
+              <span>Ainda não tem uma conta?</span>
+              <Link to="/pages/auth/register">Crie uma conta</Link>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 };
