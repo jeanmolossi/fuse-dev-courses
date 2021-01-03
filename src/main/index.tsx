ab2706e7aa195fcadb7a4568d3165f36942c3ac7
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-03 17:05:17 -0300

    chore: added global styles load

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from '@/main/routes/router';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import '@/presentation/styles/global.scss';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
