0d8e160d8aab78c058b54c58ffe1d8aa46285cce
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-03 20:36:28 -0300

    test: returns falsy if field is not empty

--- src/validation/required-field/required-field.spec.ts
+++ src/validation/required-field/required-field.spec.ts
@@ -19,4 +19,11 @@ describe('RequiredFieldValidation', () => {
     const error = sut.validate('');
     expect(error).toEqual(new RequiredFieldError(fieldName));
   });
+
+  test('Should return falsy if field is not empty', () => {
+    const { sut } = makeSut();
+
+    const error = sut.validate(faker.random.word());
+    expect(error).toBeFalsy();
+  });
 });
