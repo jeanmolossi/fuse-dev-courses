ccba82ad7491d735aed63b589a0235be9b336d32
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-05 10:04:11 -0300

    refactor: mock refactored to multiple errors

--- src/presentation/test/mock-validation.ts
+++ src/presentation/test/mock-validation.ts
@@ -1,13 +1,14 @@
 import { Validation } from '../protocols/validation';
 
 export class ValidationSpy implements Validation {
-  errorMessage: string;
+  errorMessage: string[] = [];
   fieldName: string;
   fieldValue: string | boolean | number;
 
-  validate(fieldName: string, fieldValue: string | boolean | number): string {
+  validate(fieldName: string, fieldValue: string | boolean | number): string[] {
     this.fieldName = fieldName;
     this.fieldValue = fieldValue;
+
     return this.errorMessage;
   }
 }
