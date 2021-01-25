adf6ab0701bdc421fbfade145ba25651873989d2
jeanmolossi <jean.carlosmolossi@gmail.com>
2021-01-24 22:36:53 -0300

    refactor: account model credentials from namespace

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,18 +1,20 @@
 import { HttpClient, HttpStatusCode } from '@/data/protocols/http';
 import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors';
 import { AccountModel } from '@/domain/models';
-import { AuthenticationUseCase, Credentials } from '@/domain/usecases';
+import { AuthenticationUseCase } from '@/domain/usecases';
 
 export class RemoteAuthentication implements AuthenticationUseCase {
   constructor(
     private readonly url: string,
     private readonly httpPostClient: HttpClient<
-      Credentials,
+      AccountModel.Credentials,
       AccountModel.AccessModel
     >,
   ) {}
 
-  async auth(credentials: Credentials): Promise<AccountModel.AccessModel> {
+  async auth(
+    credentials: AccountModel.Credentials,
+  ): Promise<AccountModel.AccessModel> {
     const httpRespose = await this.httpPostClient.request({
       url: this.url,
       method: 'post',
